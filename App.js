6e2115514b3483859ef0b56129eb1657713dace7
jxmijuholx <jami.juhola01@gmail.com>
2024-01-15 14:58:20 +0200

    Tehtävä valmis

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Eka react native appi</Text>
       <StatusBar style="auto" />
     </View>
   );
